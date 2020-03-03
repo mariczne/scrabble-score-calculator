a04a0feef2330cf9dfa4741d3fd93ca131b2447b
Marcin <mariczne@gmail.com>
2020-03-03 14:16:31 +0100

    Updates

---
+++ precache-manifest.2d1a0e1e3163288870af99b3411a8728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c814a015e59982306c57b955c5a260f",
+    "url": "/scrabble-score-calculator/index.html"
+  },
+  {
+    "revision": "c8f249caf9caac636863",
+    "url": "/scrabble-score-calculator/static/css/main.8ac7cd75.chunk.css"
+  },
+  {
+    "revision": "c934f0244b7370981497",
+    "url": "/scrabble-score-calculator/static/js/2.58b8f84c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/scrabble-score-calculator/static/js/2.58b8f84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f249caf9caac636863",
+    "url": "/scrabble-score-calculator/static/js/main.2a5a5093.chunk.js"
+  },
+  {
+    "revision": "875f7153dbf96193fbfd",
+    "url": "/scrabble-score-calculator/static/js/runtime-main.ad2c6d58.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/scrabble-score-calculator/precache-manifest.2d1a0e1e3163288870af99b3411a8728.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scrabble-score-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
