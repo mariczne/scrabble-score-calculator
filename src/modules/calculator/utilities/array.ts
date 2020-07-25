4566f3e33ae17b6d3e9a12fe42117bc6b961c7ce
Marcin <mariczne@gmail.com>
2020-07-25 13:26:46 +0200

    Add generic types for array utility functions

--- src/modules/calculator/utilities/array.ts
+++ src/modules/calculator/utilities/array.ts
@@ -1,8 +1,10 @@
-export function sortArrayByLengthDescending(arr: any[]): any[] {
+export function sortArrayByLengthDescending<T extends Array<any> | string>(
+  arr: T[]
+): T[] {
   return [...arr].sort((prev, curr) => curr.length - prev.length);
 }
 
-export function findIndexOfSubarray(arr: any[], subArr: any[]): number {
+export function findIndexOfSubarray<T>(arr: T[], subArr: T[]): number {
   for (let i = 0; i < 1 + (arr.length - subArr.length); ++i) {
     if (subArr.every((element, j) => element === arr[i + j])) {
       return i;
@@ -11,25 +13,22 @@ export function findIndexOfSubarray(arr: any[], subArr: any[]): number {
   return -1;
 }
 
-export function joinSubarrayIntoSingleElement(
-  arr: any[],
-  subArr: any[]
-): any[] {
+export function joinSubarrayIntoSingleElement<T>(arr: T[], subArr: T[]): T[] {
   const subArrIndexAt = findIndexOfSubarray(arr, subArr);
   if (subArrIndexAt !== -1) {
     return [
       ...arr.slice(0, subArrIndexAt),
       subArr.join(""),
       ...arr.slice(subArrIndexAt + subArr.length, arr.length),
-    ];
+    ] as T[];
   }
   return [...arr];
 }
 
-export function joinAllSubarraysIntoSingleElements(
-  arr: any[],
-  subArr: any[]
-): any[] {
+export function joinAllSubarraysIntoSingleElements<T>(
+  arr: T[],
+  subArr: T[]
+): T[] {
   const subArrIndexAt = findIndexOfSubarray(arr, subArr);
   if (subArrIndexAt === -1) {
     return [...arr];
